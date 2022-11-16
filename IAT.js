16818304b4e78991116460baeb51de8717d4ed4b
RRIC-IPS <118233825+RRIC-IPS@users.noreply.github.com>
2022-11-16 10:59:01 +0800

    Update IAT.js

--- IAT.js
+++ IAT.js
@@ -1,45 +1,46 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://RRIC-IPS.github.io/IAT/IATAPI.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
-	return iatExtension({
+return iatExtension({
 		category1 : {
-			name : 'Black people', //Will appear in the data.
+			name : 'Minorities', //Will appear in the data.
 			title : {
-				media : {word : 'Black people'}, //Name of the category presented in the task.
+				media : {word : 'Minorities'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
+    		    {image : 'Minorities1.jpg'},
+			{image : 'Minorities2.jpg'},
+			{image : 'Minorities3.jpg'}, 
+		    	{image : 'Minorities4.jpg'}, 
+    			{image : 'Minorities5.jpg'}, 
+    			{image : 'Minorities6.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'White people', //Will appear in the data.
+			name : 'Chinese', //Will appear in the data.
 			title : {
-				media : {word : 'White people'}, //Name of the category presented in the task.
+				media : {word : 'Chinese'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'yf1.jpg'}, 
-    			{image : 'yf4.jpg'}, 
-    			{image : 'yf5.jpg'}, 
-    			{image : 'ym2.jpg'}, 
-    			{image : 'ym3.jpg'}, 
-    			{image : 'ym5.jpg'}			], 
+    		    {image : 'Chinese1.jpg'},
+			{image : 'Chinese2.jpg'},
+			{image : 'Chinese3.jpg'},
+			{image : 'Chinese4.jpg'},
+		    	{image : 'Chinese5.jpg'},
+       			{image : 'Chinese6.jpg'}
+			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 
 		base_url : {//Where are your images at?
-			image : 'https://baranan.github.io/minno-tasks/images/'
+			image : 'https://RRIC-IPS.github.io/IATtest/images/'
 		} 
 	});
 });
